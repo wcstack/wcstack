df8e1c2e7b5c54cf3312de672fa507482dfbfba4
mogera551 <mogera551@coo-d.jp>
2026-02-18 07:45:01 +0900

    feat: add examples for async fetch, cart management, hello world, simple counter, simple list, and state population

---
+++ packages/state/examples/state-population/allStates.js
@@ -0,0 +1,62 @@
+
+/**
+ * @typedef {Object} StateInfo
+ * @property {string} name
+ * @property {string} capital
+ * @property {number} population
+ * @property {string} region
+ */
+
+/** @type {StateInfo[]} */
+export const allStates = [
+  {"name": "Alabama", "capital": "Montgomery", "region": "South", "population": 5108468},
+  {"name": "Alaska", "capital": "Juneau", "region": "West", "population": 733406},
+  {"name": "Arizona", "capital": "Phoenix", "region": "West", "population": 7431344},
+  {"name": "Arkansas", "capital": "Little Rock", "region": "South", "population": 3067732},
+  {"name": "California", "capital": "Sacramento", "region": "West", "population": 38965193},
+  {"name": "Colorado", "capital": "Denver", "region": "West", "population": 5877610},
+  {"name": "Connecticut", "capital": "Hartford", "region": "Northeast", "population": 3617176},
+  {"name": "Delaware", "capital": "Dover", "region": "South", "population": 1031890},
+  {"name": "Florida", "capital": "Tallahassee", "region": "South", "population": 22610726},
+  {"name": "Georgia", "capital": "Atlanta", "region": "South", "population": 11029227},
+  {"name": "Hawaii", "capital": "Honolulu", "region": "West", "population": 1435138},
+  {"name": "Idaho", "capital": "Boise", "region": "West", "population": 1964726},
+  {"name": "Illinois", "capital": "Springfield", "region": "Midwest", "population": 12549689},
+  {"name": "Indiana", "capital": "Indianapolis", "region": "Midwest", "population": 6862199},
+  {"name": "Iowa", "capital": "Des Moines", "region": "Midwest", "population": 3207004},
+  {"name": "Kansas", "capital": "Topeka", "region": "Midwest", "population": 2940546},
+  {"name": "Kentucky", "capital": "Frankfort", "region": "South", "population": 4526154},
+  {"name": "Louisiana", "capital": "Baton Rouge", "region": "South", "population": 4573749},
+  {"name": "Maine", "capital": "Augusta", "region": "Northeast", "population": 1395722},
+  {"name": "Maryland", "capital": "Annapolis", "region": "South", "population": 6180253},
+  {"name": "Massachusetts", "capital": "Boston", "region": "Northeast", "population": 7001399},
+  {"name": "Michigan", "capital": "Lansing", "region": "Midwest", "population": 10037261},
+  {"name": "Minnesota", "capital": "St. Paul", "region": "Midwest", "population": 5737915},
+  {"name": "Mississippi", "capital": "Jackson", "region": "South", "population": 2939690},
+  {"name": "Missouri", "capital": "Jefferson City", "region": "Midwest", "population": 6196156},
+  {"name": "Montana", "capital": "Helena", "region": "West", "population": 1132812},
+  {"name": "Nebraska", "capital": "Lincoln", "region": "Midwest", "population": 1978379},
+  {"name": "Nevada", "capital": "Carson City", "region": "West", "population": 3194176},
+  {"name": "New Hampshire", "capital": "Concord", "region": "Northeast", "population": 1402054},
+  {"name": "New Jersey", "capital": "Trenton", "region": "Northeast", "population": 9290841},
+  {"name": "New Mexico", "capital": "Santa Fe", "region": "West", "population": 2114371},
+  {"name": "New York", "capital": "Albany", "region": "Northeast", "population": 19571216},
+  {"name": "North Carolina", "capital": "Raleigh", "region": "South", "population": 10835491},
+  {"name": "North Dakota", "capital": "Bismarck", "region": "Midwest", "population": 783926},
+  {"name": "Ohio", "capital": "Columbus", "region": "Midwest", "population": 11785935},
+  {"name": "Oklahoma", "capital": "Oklahoma City", "region": "South", "population": 4053824},
+  {"name": "Oregon", "capital": "Salem", "region": "West", "population": 4233358},
+  {"name": "Pennsylvania", "capital": "Harrisburg", "region": "Northeast", "population": 12961683},
+  {"name": "Rhode Island", "capital": "Providence", "region": "Northeast", "population": 1095962},
+  {"name": "South Carolina", "capital": "Columbia", "region": "South", "population": 5373555},
+  {"name": "South Dakota", "capital": "Pierre", "region": "Midwest", "population": 919318},
+  {"name": "Tennessee", "capital": "Nashville", "region": "South", "population": 7126489},
+  {"name": "Texas", "capital": "Austin", "region": "South", "population": 30503301},
+  {"name": "Utah", "capital": "Salt Lake City", "region": "West", "population": 3417734},
+  {"name": "Vermont", "capital": "Montpelier", "region": "Northeast", "population": 647464},
+  {"name": "Virginia", "capital": "Richmond", "region": "South", "population": 8715698},
+  {"name": "Washington", "capital": "Olympia", "region": "West", "population": 7812880},
+  {"name": "West Virginia", "capital": "Charleston", "region": "South", "population": 1770071},
+  {"name": "Wisconsin", "capital": "Madison", "region": "Midwest", "population": 5910955},
+  {"name": "Wyoming", "capital": "Cheyenne", "region": "West", "population": 584057},
+];
